5fea7bb83da346bcb68fa825a785c72d8e427d2b
Stefanos Astaras <stefanosastaras@gmail.com>
2015-09-17 22:01:52 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Jchip8
+
+Yet another CHIP8 emulator, written in Java.
+
+It is currently under development (not functional yet). Development branch.
---
+++ src/jchip8/Jchip8.java
@@ -0,0 +1,24 @@
+package jchip8;
+
+class RegisterFile{
+    ;
+}
+
+class Display{
+    ;
+}
+
+class Timer {
+    ;
+}
+
+class SoundTimer extends Timer {
+    ;
+}
+
+public class Jchip8 {
+    public static void main(String[] args) {
+        ;
+    }
+
+}
