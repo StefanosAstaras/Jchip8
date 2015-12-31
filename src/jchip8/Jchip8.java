e208be059d802bc323b554d02c8b7c0f8d46437b
Stefanos Astaras <stefanosastaras@gmail.com>
2015-12-31 15:24:04 +0200

    Added basic Display and Keyboard scaffolding.

--- src/jchip8/Jchip8.java
+++ src/jchip8/Jchip8.java
@@ -1,34 +1,63 @@
-package jchip8;
+package Jchip8;
+import java.util.concurrent.TimeUnit;
+import java.awt.event.*;
+import javax.swing.*;
 
-class Display {
-    ;
+class Display extends JFrame
+{
+    public Display(KeyListener keyboard)
+    {
+		super("Jchip8");
+        addKeyListener(keyboard);
+        setSize(1280, 720);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
 
-class Keyboard {
-    ;
-}
-
-class Timer {
-    ;
-}
-
-class SoundTimer extends Timer {
-    ;
-}
-
-public class Jchip8 {
-    byte opcode;
-    byte[] memory = new byte[4096];
-    byte[] registers = new byte[16];
-    byte index_register;
-    byte program_counter;
-    Display screen;
-    Keyboard keys;
-    Timer delay_timer;
-    SoundTimer sound_timer;
-    
-    public static void main(String[] args) {
+class Keyboard extends JFrame implements KeyListener
+{
+    public Keyboard()
+    {
+		super();
+	}
+    public void keyPressed(KeyEvent e)
+    {
+        System.out.println("Key!");
+    }
+    public void keyReleased(KeyEvent e)
+    {
+        System.out.println("No Key!");
+    }
+    public void keyTyped(KeyEvent e)
+    {
         ;
     }
+}
 
+class Timer
+{
+    ;
+}
+
+class SoundTimer extends Timer
+{
+    ;
+}
+
+public class Jchip8
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        System.out.println("Hello, world!");
+        byte opcode;
+		byte[] memory = new byte[4096];
+		byte[] registers = new byte[16];
+		byte index_register;
+		byte program_counter;
+		Timer delay_timer;
+		SoundTimer sound_timer;
+        Keyboard keys = new Keyboard();
+        Display screen = new Display(keys);
+    }
 }
